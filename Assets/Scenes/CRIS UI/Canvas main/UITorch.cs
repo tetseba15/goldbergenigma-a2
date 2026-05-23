a11fcc138fdfd75822477ad0f38f525fe2861ad2
CrisScara <cristianscaramuzzino@gmail.com>
2026-05-23 17:38:42 -0300

    agregamos AudioSource a la bateria y el agua
    A la bateria y le agregamos el audio en su script faltaria el script del agua

--- Assets/Scenes/CRIS UI/Canvas main/UITorch.cs
+++ Assets/Scenes/CRIS UI/Canvas main/UITorch.cs
@@ -4,7 +4,8 @@ using UnityEngine.UI;
 public class UITorch : MonoBehaviour
 {
     public Slider BatterySlider;
-
+    public AudioSource BatteryEnd;
+    private bool yaSono = false;
     private void OnEnable()
     {
         GameEvent.OnBattery += UpdateBattery;
@@ -16,7 +17,24 @@ public class UITorch : MonoBehaviour
     }
     private void UpdateBattery(float current, float max)
     {
-        BatterySlider.value = (current / max);
+        float porcentaje = current / max;
+        BatterySlider.value = porcentaje;
+
+        if (porcentaje <= 0f)
+        {
+            // Solo reproduce si no ha sonado antes
+            if (!yaSono && BatteryEnd != null)
+            {
+                BatteryEnd.Play();
+                yaSono = true; // Marcamos que ya se reprodujo
+            }
+        }
+        else
+        {
+            // Si la batería vuelve a cargarse o sube de 0, reiniciamos el estado
+            yaSono = false;
+        }
 
     }
+
 }
