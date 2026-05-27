a8985e0415839270847385d6d23017609075a9fd
RTadan997 <adan_rada17@hotmail.com>
2026-05-27 03:18:24 -0300

    Shovel System
    Agregado:
    
    - Pala en la chimenea
    - Sistema de la pala: interactúa con la chimenea y la tumba
    - Animación de la pala

---
+++ Assets/Scripts/PlayerShovel.cs
@@ -0,0 +1,89 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class ShovelController : MonoBehaviour
+{
+    [Header("Referencias")]
+    [SerializeField] private PlayerInventory _inventory;
+    [SerializeField] private GameObject _shovelVisual;
+    [SerializeField] private Animator _shovelAnimator;
+    [SerializeField] private Camera _mainCamera; 
+
+    [Header("Audio")]
+    [SerializeField] private AudioSource _audioSource;
+    [SerializeField] private AudioClip _shovelSound;
+
+    [Header("Configuración")]
+    [SerializeField] private float _animationDuration;
+    [SerializeField] private float _interactionDistance; 
+    [SerializeField] private LayerMask _interactableMask; 
+
+    private bool _isUsing = false;
+
+    void Start()
+    {
+        if (_shovelVisual != null)
+        {
+            _shovelVisual.SetActive(false);
+        }
+
+        
+        if (_mainCamera == null)
+        {
+            _mainCamera = Camera.main;
+        }
+    }
+
+    void Update()
+    {
+        if (Keyboard.current == null) return;
+
+        
+        if (Keyboard.current.gKey.wasPressedThisFrame)
+        {
+            
+            bool tienePala = _inventory.HasItem(PlayerInventory.ItemType.Shovel);
+
+            if (tienePala && !_isUsing)
+            {
+                TryInteractWithShovel();
+            }
+        }
+    }
+
+    private void TryInteractWithShovel()
+    {
+        Ray ray = _mainCamera.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0));
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit, _interactionDistance, _interactableMask))
+        {
+            if (hit.collider.CompareTag("Chimney") || hit.collider.CompareTag("Grave"))
+            {
+                StartCoroutine(UseShovelRoutine(hit.collider.gameObject));
+            }
+        }
+    }
+
+    private IEnumerator UseShovelRoutine(GameObject targetObject)
+    {
+        _isUsing = true;
+        _shovelVisual.SetActive(true);
+
+        if (_audioSource != null && _shovelSound != null)
+        {
+            _audioSource.PlayOneShot(_shovelSound);
+        }
+
+        if (_shovelAnimator != null)
+        {
+            _shovelAnimator.SetTrigger("Dig");
+        }
+
+        yield return new WaitForSeconds(_animationDuration);
+
+        _shovelVisual.SetActive(false);
+        _isUsing = false;
+    }
+}
