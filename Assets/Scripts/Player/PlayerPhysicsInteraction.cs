1f0e6c9e79abbb7534be0f9fd81c1856fff42874
Sebastian Gomez <tetseba15@gmail.com>
2026-05-30 18:03:11 -0300

    Agregados SFX y ajustando valores del peso

--- Assets/Scripts/Player/PlayerPhysicsInteraction.cs
+++ Assets/Scripts/Player/PlayerPhysicsInteraction.cs
@@ -3,20 +3,31 @@ using UnityEngine;
 [RequireComponent(typeof(CharacterController))]
 public class PlayerPhysicsInteraction : MonoBehaviour
 {
+    [Header("References")]
+    [SerializeField] private PlayerInputHandler _playerInput;
+
+
     [Header("Push Settings")]
     [SerializeField, Tooltip("Fuerza base con la que el jugador empuja objetos")]
     private float _pushForce = 2f;
+
 
     private void OnControllerColliderHit(ControllerColliderHit hit)
     {
         Rigidbody body = hit.collider.attachedRigidbody;
 
-        if (body == null || body.isKinematic) return;
+        if (body == null || body.isKinematic || hit.moveDirection.y < -0.3f) return;
 
-        if (hit.moveDirection.y < -0.3f) return;
+        Vector3 pushDirection = new Vector3(hit.moveDirection.x, 0, hit.moveDirection.z).normalized;
+        
 
-        Vector3 pushDirection = new Vector3(hit.moveDirection.x, 0, hit.moveDirection.z);
-
-        body.AddForceAtPosition(pushDirection * _pushForce, hit.point, ForceMode.Impulse);
+        if (_playerInput.IsSprinting)
+        {
+            body.AddForceAtPosition(pushDirection * _pushForce * 3, hit.point, ForceMode.Impulse);
+        }
+        else
+        {
+            body.AddForceAtPosition(pushDirection * _pushForce, hit.point, ForceMode.Impulse);
+        }
     }
 }
