77f9f0a21c066ee3a6b17b6737f78f871a709a8e
CrisScara <cristianscaramuzzino@gmail.com>
2026-06-01 19:03:29 -0300

    Audio mixer
    se hizo un Audio mixer para hacer un snapshot así no se corta la música tan seco, y sacar la música al entrar a la casa. Se creo tambien un object empty que funciona como trigger para activar el snapshot de la musica.

--- Assets/Scripts/Sfx-Ambience/SoundAmbience1.cs
+++ Assets/Scripts/Sfx-Ambience/SoundAmbience1.cs
@@ -7,31 +7,16 @@ public class SoundAmbience1 : MonoBehaviour
 
     void Start()
     {
-        // Configuramos el audio para que repita en bucle y sea envolvente (2D)
+        
         audioSource.clip = ambientAudio;
         audioSource.loop = true;
         audioSource.spatialBlend = 0f;
+
+        if (!audioSource.isPlaying)
+        {
+            audioSource.Play();
+        }
     }
 
-    // Se activa cuando el jugador entra al espacio exterior
-    private void OnTriggerEnter(Collider other)
-    {
-        if (other.CompareTag("Player"))
-        {
-            if (!audioSource.isPlaying)
-            {
-                audioSource.Play();
-            }
-        }
-    }
-    private void OnTriggerExit(Collider other)
-    {
-        if (other.CompareTag("Player"))
-        {
-            if (audioSource.isPlaying)
-            {
-                audioSource.Stop(); // Apaga el sonido exterior de golpe
-            }
-        }
-    }
+    
 }
