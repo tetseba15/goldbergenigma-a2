f792a5b00ea17c3ba08cd6e2d35d91f177399cf1
Juan Ignacio <juanirabe900@gmail.com>
2026-06-05 14:15:31 -0300

    Fix warning en DialogueTrigger

--- Assets/Scripts/Systems/DialogueTrigger.cs
+++ Assets/Scripts/Systems/DialogueTrigger.cs
@@ -1,25 +1,21 @@
-using Unity.Burst;
 using UnityEngine;
 
 public class DialogueTrigger : MonoBehaviour
 {
     [SerializeField] private string _dialogue = "Insert Dialogue";
-    [SerializeField, Range(1,3)]
+    [SerializeField, Range(1, 3)]
     private int _triggerOnAct;
-
-    [SerializeField] private PlayerInventory.ItemType item;
+    [SerializeField] private bool _requiresItem = false;
+    [SerializeField] private PlayerInventory.ItemType _requiredItem;
 
     private void OnTriggerEnter(Collider other)
     {
         PlayerInventory inventory = other.GetComponent<PlayerInventory>();
-        if (
-            inventory != null &&
-            _triggerOnAct == OuijaBoard.Instance.CurrentAct &&
-            (item == null || inventory.HasItem(PlayerInventory.ItemType.WorkshopKey))
-        )
-        {
-            DialogueManager.Instance.ShowDialogue(_dialogue);
-            gameObject.SetActive(false);
-        }
+        if (inventory == null) return;
+        if (_triggerOnAct != OuijaBoard.Instance.CurrentAct) return;
+        if (_requiresItem && !inventory.HasItem(_requiredItem)) return;
+
+        DialogueManager.Instance.ShowDialogue(_dialogue);
+        gameObject.SetActive(false);
     }
 }
