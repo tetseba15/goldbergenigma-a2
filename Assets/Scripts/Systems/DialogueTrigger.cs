ab25a3927c1b11822efe57bf1f74c3842c617428
Ezequiel Frisone <ezequielfrisone@gmail.com>
2026-06-28 13:49:55 -0300

    Desacopla DialogueTrigger.cs de OuijaBoard

--- Assets/Scripts/Systems/DialogueTrigger.cs
+++ Assets/Scripts/Systems/DialogueTrigger.cs
@@ -2,17 +2,18 @@ using UnityEngine;
 
 public class DialogueTrigger : MonoBehaviour
 {
+    [Header("Dialogue")]
     [SerializeField] private string _dialogue = "Insert Dialogue";
-    [SerializeField, Range(1, 3)]
-    private int _triggerOnAct;
+
+    [Header("Conditions")]
+    [SerializeField] private GameFlowManager.Act _triggerOnAct;
     [SerializeField] private bool _requiresItem = false;
     [SerializeField] private PlayerInventory.ItemType _requiredItem;
 
     private void OnTriggerEnter(Collider other)
     {
         PlayerInventory inventory = other.GetComponent<PlayerInventory>();
-        if (inventory == null) return;
-        //if (_triggerOnAct != OuijaBoard.Instance.CurrentAct) return;
+        if (inventory == null || GameFlowManager.Instance.CurrentAct != _triggerOnAct) return;
         if (_requiresItem && !inventory.HasItem(_requiredItem)) return;
 
         DialogueManager.Instance.ShowDialogue(_dialogue);
