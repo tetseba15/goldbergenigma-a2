645f420ab2474e97075fbdba43b230fa7eb8e4a2
Ezequiel Frisone <ezequielfrisone@gmail.com>
2026-07-04 14:58:16 -0300

    Agrega susurros y modifica cambio de foto
    Para el trigger del comedor:
    
    Agrega susurros
    Se da la opción de volver a la foto original. Si es así, se modifica el tiempo que se mantiene la nueva foto.
    Vuelve al offset original del material al volver a la foto original.

--- Assets/Scripts/Systems/ChangingPicture.cs
+++ Assets/Scripts/Systems/ChangingPicture.cs
@@ -2,34 +2,73 @@ using UnityEngine;
 
 public class ChangingPicture : MonoBehaviour
 {
-    [Header("Texture")]
+    [Header("Texture Settings")]
     [SerializeField] private Texture2D _texture;
     [SerializeField] private Vector2 _textureOffset;
+    [SerializeField]
+    [Tooltip("Cantidad de tiempo que mantiene la nueva textura (de ser 0 no vuelve a la textura original)")] private float _changedTime;
 
     [Header("Game Event Type")]
     [SerializeField] private GenericEventsTrigger.GameEventType _gameEventType;
 
+    private Renderer _renderer;
+    private Texture2D _originTexture;
+    private Texture2D _actualTexture;
+
+    private Vector2 _originOffset;
+
+    private float _time;
+
+    private void Awake()
+    {
+        _renderer = GetComponent<Renderer>();
+        _originTexture = GetComponent<Renderer>().material.mainTexture as Texture2D;
+        _actualTexture = _originTexture;
+
+        _originOffset = _renderer.material.GetTextureOffset("_BaseMap");
+    }
+
+    private void Update()
+    {
+        if (_changedTime == 0) return;
+
+        if (_actualTexture != _originTexture)
+        {
+            _time += Time.deltaTime;
+
+            if (_time >= _changedTime)
+            {
+                ChangePicture(_originTexture);
+                _renderer.material.SetTextureOffset("_BaseMap", _originOffset);
+            }
+        }
+    }
+
     private void OnEnable()
     {
-        GenericEventsTrigger.OnTriggerEvent += ChangePicture;
+        GenericEventsTrigger.OnTriggerEvent += TriggerChange;
     }
 
     private void OnDisable()
     {
-        GenericEventsTrigger.OnTriggerEvent -= ChangePicture;
+        GenericEventsTrigger.OnTriggerEvent -= TriggerChange;
     }
 
-    private void ChangePicture(GenericEventsTrigger.GameEventType gameEventType)
+    private void TriggerChange(GenericEventsTrigger.GameEventType gameEventType)
     {
         if (gameEventType == _gameEventType)
         {
-            if (_texture != null)
-            {
-                Renderer renderer = GetComponent<Renderer>();
-                renderer.material.SetTexture("_BaseMap", _texture);
-                renderer.material.SetTextureOffset("_BaseMap", new(_textureOffset.x, _textureOffset.y));
-            }
-               
+            ChangePicture(_texture);
+            _renderer.material.SetTextureOffset("_BaseMap", new(_textureOffset.x, _textureOffset.y));
+        }
+    }
+
+    private void ChangePicture(Texture2D texture)
+    {
+        if (texture != null)
+        {
+            _renderer.material.SetTexture("_BaseMap", texture);
+            _actualTexture = texture;
         }
     }
 }
