b7d0f7d539408f6adc7b89bef53c0e4740c038ac
Ezequiel Frisone <ezequielfrisone@gmail.com>
2026-07-08 19:21:59 -0300

    Esconde la barra de fe luego de un tiempo
    Al usar fe se muestra la barra.
    
    Cuando la barra se está recargando se muestra en pantalla.
    
    Luego de cargarse completamente se vuelve a esconder.

--- Assets/Scripts/UI/FaithBar.cs
+++ Assets/Scripts/UI/FaithBar.cs
@@ -9,12 +9,18 @@ public class FaithBar : MonoBehaviour
 
     [Header("Slider Settings")]
     [SerializeField] private float _sliderUpdateSpeed = 3f;
+    [SerializeField] private float _timeToDissapear = 2f;
+    [SerializeField] private float _fadeSpeed = 1.5f;
 
     private Slider _slider;
+    private CanvasGroup _canvasGroup;
+
+    private float _dissapearTime;
 
     private void Awake()
     {
         _slider = GetComponent<Slider>();
+        _canvasGroup = GetComponent<CanvasGroup>();
     }
 
     private void OnEnable()
@@ -27,9 +33,35 @@ public class FaithBar : MonoBehaviour
         FaithController.OnFaithChange -= UpdateFaithBar;
     }
 
+    private void Update()
+    {
+        if (_canvasGroup != null && _canvasGroup.alpha == 1)
+        {
+            _dissapearTime += Time.deltaTime;
+            if (_dissapearTime >= _timeToDissapear)
+            {
+                StartCoroutine(UpdateSliderAlpha(0));
+                _dissapearTime = 0;
+            }
+        }
+    }
+
     private void UpdateFaithBar(float actualFaith)
     {
         StartCoroutine(MoveTowardsValue(actualFaith));
+        StartCoroutine(UpdateSliderAlpha(1));
+        _dissapearTime = 0;
+    }
+
+    private IEnumerator UpdateSliderAlpha(float alphaDestiny)
+    {
+        while (true)
+        {
+            _canvasGroup.alpha = Mathf.MoveTowards(_canvasGroup.alpha, alphaDestiny, _fadeSpeed * Time.deltaTime);
+
+            if (_canvasGroup.alpha == alphaDestiny) yield break;
+            yield return null;
+        }
     }
 
     private IEnumerator MoveTowardsValue(float actualFaith)
@@ -39,7 +71,6 @@ public class FaithBar : MonoBehaviour
             _slider.value = Mathf.MoveTowards(_slider.value, actualFaith, _sliderUpdateSpeed * Time.deltaTime);
 
             if (_slider.value == actualFaith) yield break;
-
             yield return null;
         }
     }
